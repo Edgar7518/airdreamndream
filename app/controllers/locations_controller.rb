b082337ba67ddc230cc4bbaefa4dfb4279380481
concordenzeyimana <nzeconco@gmail.com>
2022-11-29 17:01:55 -0500

    added associations to user

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,25 +1,11 @@
 class LocationsController < ApplicationController
   def index
-    @locations = Location.all
   end
 
   def new
-    @dream = Dream.find(params[:dream_id])
-    @location = Location.new
   end
 
   def create
-    @location = Location.new(location_params)
-    @dream = Dream.find(params[:dream_id])
-    @location.dream = @dream
-    @location.user = current_user
-
-    if @location.save
-      redirect_to locations_path
-    else
-      raise
-      render :new, status: :unprocessable_entity
-    end
   end
 
   def update
@@ -34,10 +20,5 @@ class LocationsController < ApplicationController
 
   def location_params
     params.require(:location).permit(:comment, :status)
-  end
-
-  private
-  def location_params
-    params.require(:location).permit([:comment])
   end
 end
