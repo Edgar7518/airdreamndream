570a2b9005333c48edac1d550a2ee813bfb8d47e
estelle rambur <estelle.rambur@gmail.com>
2022-11-29 16:20:37 -0500

    A user can create a location

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -9,16 +9,24 @@ class LocationsController < ApplicationController
   end
 
   def create
-    @location = Location.new
+    @location = Location.new(location_params)
     @dream = Dream.find(params[:dream_id])
     @location.dream = @dream
+    @location.user = current_user
+
     if @location.save
-      redirect_to dream_location_path(@dream)
+      redirect_to locations_path
     else
+      raise
       render :new, status: :unprocessable_entity
     end
   end
 
   def update
+  end
+
+  private
+  def location_params
+    params.require(:location).permit([:comment])
   end
 end
