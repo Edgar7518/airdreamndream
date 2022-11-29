2074fc18fbcad2b88afdacb2d9cf083e95d341e8
estelle rambur <estelle.rambur@gmail.com>
2022-11-29 17:13:50 -0500

    create action

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,24 +1,32 @@
 class LocationsController < ApplicationController
   def index
+    @locations = Location.all
   end
 
   def new
+    @dream = Dream.find(params[:dream_id])
+    @location = Location.new
   end
 
   def create
-  end
+    @location = Location.new(location_params)
+    @dream = Dream.find(params[:dream_id])
+    @location.dream = @dream
+    @location.user = current_user
 
-  def update
-    if @location.update(location_params)
-      redirect_to @location, notice: "Your rental was successfully updated."
+    if @location.save
+      redirect_to locations_path
     else
-      render :edit, status: :unprocessable_entity
+      raise
+      render :new, status: :unprocessable_entity
     end
   end
 
-  private
+  def update
+  end
 
+  private
   def location_params
-    params.require(:location).permit(:comment, :status)
+    params.require(:location).permit([:comment])
   end
 end
