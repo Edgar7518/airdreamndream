07ee14aba4bfcf215908f1049d6c12e660c6d2b1
concordenzeyimana <nzeconco@gmail.com>
2022-11-29 15:39:48 -0500

    update locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -9,5 +9,16 @@ class LocationsController < ApplicationController
   end
 
   def update
+    if @location.update(location_params)
+      redirect_to @location, notice: "Your rental was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:comment, :status)
   end
 end
