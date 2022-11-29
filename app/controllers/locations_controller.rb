05197d4024954436cefd6a573c5f19b2200f59ae
estelle rambur <estelle.rambur@gmail.com>
2022-11-29 15:21:54 -0500

    Action new + create locations view to add a comment

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,9 +3,19 @@ class LocationsController < ApplicationController
   end
 
   def new
+    @dream = Dream.find(params[:dream_id])
+    @location = Location.new
   end
 
   def create
+    @location = Location.new
+    @dream = Dream.find(params[:dream_id])
+    @location.dream = @dream
+    if @location.save
+      redirect_to dream_location_path(@dream)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
