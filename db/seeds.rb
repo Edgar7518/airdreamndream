d22e81b46e6fc463ec30ec78fe4215ec3096b9d6
estelle rambur <estelle.rambur@gmail.com>
2022-12-01 12:09:23 -0500

    update seed with good photo

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,10 @@
 #   Character.create(name: "Luke", movie: movies.first)
 require "open-uri"
 
-# file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
-# dream = Dream.new(name: "NES", body: "A great console")
-# dream.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
-# dream.save
+
 print "Cleaning..."
 
-# Location.destroy_all
+Location.destroy_all
 Dream.destroy_all
 Category.destroy_all
 User.destroy_all
@@ -38,202 +35,174 @@ end
 
 puts "done!"
 print "creating horror dreams..."
+all_dreams = [
   Dream.create!(
-    name: "Lost in the woods",
-    description: "One night lost in the woods while you try to impress the girl you love",
+    name: "Perdu dans les bois",
+    description: "Tu es perdu dans les bois, seul(e) et tu rencontre le grand méchant loup qui veut te manger. Heureusement tu es sauvé par une famille de cochons",
     price: 50,
     duration: 45,
     category: Category.find_by(name: "horror"),
     user: User.find_by(email: "yuhecny@gmail.com")
-  )
+  ),
 
-puts "done!"
-print "creating horror dreams..."
+
   Dream.create!(
-    name: "A day with your mother in law",
-    description: "Fulfill your dream of bothering your mother-in-law as if it were in real life",
+    name: "J'ai perdu mes dents",
+    description: "Je me réveille avec une drôle sensation dans la bouche, toutes mes dents ont disparues !",
     price: 500,
     duration: 2,
     category: Category.find_by(name: "horror"),
     user: User.find_by(email: "yuhecny@gmail.com")
-  )
+  ),
 
-puts "done!"
-print "creating coding dreams..."
+
   Dream.create!(
-    name: "coding in one day",
-    description: "Coding a complete application in one day without any errors",
+    name: "Coder une application en une journée",
+    description: "De viens un super développeur en codant une applicattion complète en seulement une journée",
     price: 1000,
     duration: 2,
     category: Category.find_by(name: "coding"),
     user: User.find_by(email: "estelle@gmail.com")
-  )
+  ),
 
-puts "done!"
-print "creating coding dreams..."
   Dream.create!(
-    name: "coding life",
-    description: "Be the best programmer you can create your life with just one app",
+    name: "Une vie de code",
+    description: "tu es le meilleur développeur web",
     price: 45,
     duration: 2,
     category: Category.find_by(name: "coding"),
     user: User.find_by(email: "estelle@gmail.com")
-  )
+  ),
 
-puts "done!"
-print "creating romantic dreams..."
   Dream.create!(
-    name: "The perfect dates",
-    description: "Five dates with five different people in the same day",
+    name: "Les dates parfaites",
+    description: "Participe à cinq dates en une journée avec cinq personnes différentes",
     price: 150,
     duration: 5,
     category: Category.find_by(name: "romantic"),
     user: User.find_by(email: "edgar@gmail.com")
-  )
+  ),
 
-puts "done!"
-print "creating romantic dreams..."
+
   Dream.create!(
-    name: "A picnic with your crush",
-    description: "Sunny day to have a picnic in the central park with your crush",
+    name: "Mariage avec un chat",
+    description: "Coup de foudre avec le plus beau des chats, tu décides de te marier avec lui tout de suite",
     price: 100,
     duration: 2,
     category: Category.find_by(name: "romantic"),
     user: User.find_by(email: "edgar@gmail.com")
-  )
+  ),
 
-puts "done!"
-print "creating travel dreams..."
   Dream.create!(
-    name: "Trip to thailand",
-    description: "A relaxing week trip on the beaches of thailand with your favorite person",
+    name: "Voyage en Thaïland",
+    description: "Un weekend sur les plages thaïlandaises en bonne compagnie",
     price: 500,
     duration: 4,
     category: Category.find_by(name: "travel"),
     user: User.find_by(email: "concorde@gmail.com")
-  )
+  ),
 
-puts "done!"
-print "creating travel dreams..."
   Dream.create!(
-    name: "Travel to europe by train with your best friends",
-    description: "The trip you always wanted in the company of your three best friends",
+    name: "Voyager à travers l'Europe",
+    description: "Tu pars en voyage avec tes amis pour visiter tous les pays d'Europe",
     price: 40,
     duration: 4,
     category: Category.find_by(name: "travel"),
     user: User.find_by(email: "concorde@gmail.com")
-  )
+  ),
 
-puts "done!"
-print "eating dreams..."
   Dream.create!(
-    name: "All you can eat",
-    description: "All night eating at your favorite restaurants, without ending up sick",
+    name: "Tout manger",
+    description: "Tu passes toute la journée à manger tes plats préférés jusqu'à épuisement ",
     price: 60,
     duration: 4,
     category: Category.find_by(name: "eating"),
     user: User.find_by(email: "concorde@gmail.com")
-  )
+  ),
 
-puts "done!"
-print "eating dreams..."
   Dream.create!(
-    name: "Cooking class",
-    description: "Tired of your food burning? Have a cooking class with the most famous chef in montreal, just for you",
+    name: "Cours de cuisine",
+    description: "Tu es fatigué de toujours manger la même chose ? Participe à un cours de cuisine avec un grand Chef",
     price: 35,
     duration: 3,
     category: Category.find_by(name: "eating"),
     user: User.find_by(email: "concorde@gmail.com")
-  )
+  ),
 
-puts "done!"
-print "creating sport dreams..."
   Dream.create!(
-    name: "Match Tenis",
-    description: "Beat the number one tennis player Roger Federer",
+    name: "Match de tennis",
+    description: "Tu remportes la Coupe Davis face aà Rafael Nadal, facile !",
     price: 250,
     duration: 3,
     category: Category.find_by(name: "sport"),
     user: User.find_by(email: "yuhecny@gmail.com")
-  )
+  ),
 
-puts "done!"
-print "creating sport dreams..."
+
   Dream.create!(
-    name: "The world Cup",
-    description: "Live the world cup from your dreams in the comfort of your home, close your eyes and go directly to qatar and play with your favorite team",
+    name: "Coupe du monde soccer",
+    description: "Tu pars au Qatar pour participer à la coupe du monde de soccer avec l'équipe du Luxembourg et remporte la coupe",
     price: 120,
     duration: 2,
     category: Category.find_by(name: "sport"),
     user: User.find_by(email: "yuhecny@gmail.com")
-  )
-
-puts "done!"
-print "creating concerts dreams..."
+  ),
+#////////////////
   Dream.create!(
-    name: "Mariah carey concert",
-    description: "Backstage tickets and the chance to meet Mariah Carey before seeing her concert in the front row",
+    name: "Noël avec Mariah Carey",
+    description: "Décore ton sapin de noël en compagnie de Mariah Carey, tout en écoutant le titre All I want for christmas is you",
     price: 80,
     duration: 2,
     category: Category.find_by(name: "concerts"),
     user: User.find_by(email: "edgar@gmail.com")
-  )
+  ),
 
-puts "done!"
-print "creating retirement dreams..."
   Dream.create!(
-    name: "A year on a retiree cruise",
-    description: "The retirement of your dreams in the perfect cruise with your retired friends, drinking mimosas and playing poker",
+    name: "Une année avec Tom Cruis",
+    description: "Des vacances aux côtés de Tom, manger dans de bons restaurants et visiter le monde",
     price: 350,
     duration: 2,
     category: Category.find_by(name: "retirement"),
     user: User.find_by(email: "concorde@gmail.com")
-  )
+  ),
 
-puts "done!"
-print "time dreams..."
   Dream.create!(
-    name: "Return to the past",
-    description: "Go back to your favorite childhood day",
+    name: "Retourner dans le passé",
+    description: "Tu peux revivre les meilleurs moments de ton enfance",
     price: 80,
     duration: 3,
     category: Category.find_by(name: "time"),
     user: User.find_by(email: "edgar@gmail.com")
-  )
+  ),
 
-puts "done!"
-print "gladiator dreams..."
   Dream.create!(
-    name: "Gladiator fights",
-    description: "Fight between gladiators and stay with the title of the best warrior",
+    name: "Combat de Gladiateurs ",
+    description: "Lutte sans merci entre gladiateurs et tu remportes le combat avec honneur",
     price: 30,
     duration: 1,
     category: Category.find_by(name: "gladiator"),
     user: User.find_by(email: "edgar@gmail.com")
-  )
+  ),
 
-puts "done!"
-print "movie dreams..."
   Dream.create!(
-    name: "I am a legend",
-    description: "Be the protagonist of the in the movies of your dreamsa, movie with the actor of your choice",
+    name: "Je suis une légende",
+    description: "Tu es l'acteur principal dans ton film préféré",
     price: 50,
     duration: 1,
     category: Category.find_by(name: "movie"),
     user: User.find_by(email: "yuhecny@gmail.com")
-  )
+  ),
 
-puts "done!"
-print "erotic..."
+
   Dream.create!(
-    name: "One night in paradise",
-    description: "The experience you need to have a different night",
+    name: "Comme un oiseau",
+    description: "Tu as un super pouvoir qui te permet de voler comme un oiseau",
     price: 90,
     duration: 2,
-    category: Category.find_by(name: "erotic"),
+    category: Category.find_by(name: "travel"),
     user: User.find_by(email: "yuhecny@gmail.com")
   )
-
+]
 puts "done!"
 
 # Location.create(
@@ -243,5 +212,62 @@ puts "done!"
 #   status: 0
 # )
 
-puts "All done!"
+dream_image_0 = URI.open("https://res.cloudinary.com/dvw6upciw/image/upload/v1669911925/perdu_dans_les_bois_riq86s.jpg")
+all_dreams[0].photo.attach(io: dream_image_0, filename: "", content_type: "image/png")
 
+dream_image_1 = URI.open("https://res.cloudinary.com/dvw6upciw/image/upload/v1669911925/perdre-ses-dents_f50gmg.jpg")
+all_dreams[1].photo.attach(io: dream_image_1, filename: "", content_type: "image/png")
+
+dream_image_2 = URI.open("https://res.cloudinary.com/dvw6upciw/image/upload/v1669912619/web_dev_gewzss.jpg")
+all_dreams[2].photo.attach(io: dream_image_2, filename: "", content_type: "image/png")
+
+dream_image_3 = URI.open("https://res.cloudinary.com/dvw6upciw/image/upload/v1669912655/iStock-1017296544_igqz1c.jpg")
+all_dreams[3].photo.attach(io: dream_image_3, filename: "", content_type: "image/png")
+
+dream_image_4 = URI.open("https://res.cloudinary.com/dvw6upciw/image/upload/v1669912619/5_dates_rourdm.jpg")
+all_dreams[4].photo.attach(io: dream_image_4, filename: "", content_type: "image/png")
+
+dream_image_5 = URI.open("https://res.cloudinary.com/dvw6upciw/image/upload/v1669911925/marier_avec_un_chat_piozqd.jpg")
+all_dreams[5].photo.attach(io: dream_image_5, filename: "", content_type: "image/png")
+
+dream_image_6 = URI.open("https://res.cloudinary.com/dvw6upciw/image/upload/v1669912940/thailande_kyffys.jpg")
+all_dreams[6].photo.attach(io: dream_image_6, filename: "", content_type: "image/png")
+
+dream_image_7 = URI.open("https://res.cloudinary.com/dvw6upciw/image/upload/v1669912941/europe_cp1lj9.jpg")
+all_dreams[7].photo.attach(io: dream_image_7, filename: "", content_type: "image/png")
+
+
+dream_image_8 = URI.open("https://res.cloudinary.com/dvw6upciw/image/upload/v1669913734/tout_manger_yyyrkv.webp")
+all_dreams[8].photo.attach(io: dream_image_8, filename: "", content_type: "image/png")
+
+dream_image_9 = URI.open("https://res.cloudinary.com/dvw6upciw/image/upload/v1669913734/cours_cuisine_s2ukqi.jpg")
+all_dreams[9].photo.attach(io: dream_image_9, filename: "", content_type: "image/png")
+
+dream_image_10 = URI.open("https://res.cloudinary.com/dvw6upciw/image/upload/v1669913734/trnnis_oserp2.jpg")
+all_dreams[10].photo.attach(io: dream_image_10, filename: "", content_type: "image/png")
+
+
+dream_image_11 = URI.open("https://res.cloudinary.com/dvw6upciw/image/upload/v1669913734/soccer_ot7rm9.jpg")
+all_dreams[11].photo.attach(io: dream_image_11, filename: "", content_type: "image/png")
+
+
+dream_image_12 = URI.open("https://res.cloudinary.com/dvw6upciw/image/upload/v1669911925/Mariah_carey_rzmizv.jpg")
+all_dreams[12].photo.attach(io: dream_image_12, filename: "", content_type: "image/png")
+
+dream_image_13 = URI.open("https://res.cloudinary.com/dvw6upciw/image/upload/v1669913767/tom_cruise_zqzwn6.jpg")
+all_dreams[13].photo.attach(io: dream_image_13, filename: "", content_type: "image/png")
+
+dream_image_14 = URI.open("https://res.cloudinary.com/dvw6upciw/image/upload/v1669914185/passe_rpppac.jpg")
+all_dreams[14].photo.attach(io: dream_image_14, filename: "", content_type: "image/png")
+
+dream_image_15 = URI.open("https://res.cloudinary.com/dvw6upciw/image/upload/v1669914186/gladiateur_nzhcqs.jpg")
+all_dreams[15].photo.attach(io: dream_image_15, filename: "", content_type: "image/png")
+
+dream_image_16 = URI.open("https://res.cloudinary.com/dvw6upciw/image/upload/v1669914185/acteur_legende_zjazlv.webp")
+all_dreams[16].photo.attach(io: dream_image_16, filename: "", content_type: "image/png")
+
+dream_image_17 = URI.open("https://res.cloudinary.com/dvw6upciw/image/upload/v1669913734/voler_ovqote.jpg")
+all_dreams[17].photo.attach(io: dream_image_17, filename: "", content_type: "image/png")
+
+
+puts "All done!"
