1b84d984b4258822c063018b9879b52031d4dd8a
estelle rambur <estelle.rambur@gmail.com>
2022-12-02 15:38:46 -0500

    finalisation front end

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   def home
-    @dream = Dream.find_by(name:"J'ai perdu mes dents")
-    @dreamTwo = Dream.find_by(name:"Noël avec Mariah Carey")
-    @dreamThree = Dream.find_by(name:"Mariage avec un chat")
+    @dream = Dream.find_by(name: "J'ai perdu mes dents")
+    @dreamTwo = Dream.find_by(name: "Noël avec Mariah Carey")
+    @dreamThree = Dream.find_by(name: "Mariage avec un chat")
 
   end
 end
